8d10821994037494aa107a3c2f97ba338a149189
Matias Quintana <mquintana@altimetrik.com>
2019-06-24 15:35:08 -0300

    Moved logic to helper. Also small styles changes were made.

--- force-app/main/default/aura/BoardItem/BoardItemController.js
+++ force-app/main/default/aura/BoardItem/BoardItemController.js
@@ -1,49 +1,26 @@
 ({
-    doInit : function(cmp, event, helper) {
-        
+    doInit : function(cmp, event, helper){
+        var item = cmp.get("v.item");
+        if(item.Habit_Failed__c){
+            cmp.set("v.subtractHits", 1);
+        }else if(item.Task_Completed__c){
+            cmp.set("v.addHits", 1);
+        }
     },
 
     handleSelectStatus : function(cmp, event, helper){
-        var selectedStatus= event.getParam("value");
-        var compEvent = cmp.getEvent("changeTaskStatusEvent");
-        var task = cmp.get("v.item");
-        compEvent.setParams({
-            "taskId" : task.Id, 
-            "taskNewStatus" : selectedStatus
-        });
-        compEvent.fire();
+        helper.fireChangeStatusEvent(cmp, event.getParam("value"));
     },
 
     navigateToTask : function(cmp, event, helper){
-        var navEvt = $A.get("e.force:navigateToSObject");
-        navEvt.setParams({
-            "recordId": cmp.get("v.item").Id
-        });
-        navEvt.fire();
+        helper.navigateToRecord(cmp.get("v.item").Id);
     },
 
     addPoints : function(cmp, event, helper){
-        var compEvent = cmp.getEvent("changeHabitPointsEvent");
-        var task = cmp.get("v.item");
-        compEvent.setParams({
-            "taskId" : task.Id, 
-            "taskAction" : 'add'
-        });
-        compEvent.fire();  
-        cmp.set("v.addHits", cmp.get("v.addHits")+1);
-        cmp.set("v.item.Task_Completed__c", true);  
+        helper.changePoints(cmp, 'add');
     },
 
     subtractPoints : function(cmp, event, helper){
-        var compEvent = cmp.getEvent("changeHabitPointsEvent");
-        var task = cmp.get("v.item");
-        compEvent.setParams({
-            "taskId" : task.Id, 
-            "taskAction" : 'subtract'
-        });
-        compEvent.fire();
-        cmp.set("v.subtractHits", cmp.get("v.subtractHits")+1);
-        cmp.set("v.item.Habit_Failed__c", true); 
-        cmp.set("v.item.Task_Completed__c", true); 
+        helper.changePoints(cmp, 'subtract');
     }
 })
